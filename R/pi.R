95bf133e7414741ef4f7a21edb70c972d22ebf60
Lisa <kossmannlisa03@gmail.com>
2025-08-24 11:08:50 +0200

    fix function

--- R/pi.R
+++ R/pi.R
@@ -10,5 +10,10 @@
 #‘ @examples
 #‘ lower_pi(norm(100))
 lower_pi <- function(values, CI = 0.97) {
+  # Checking validity for CI value
+  if (!(is.double(CI) && length(CI) == 1 && !is.na(CI) && CI>0.0 && CI<1.0)) {
+    stop("`CI` must be a single floating-point (double) number in 0..1 range.")
+  }
+
   unname(stats::quantile(values, (1 - CI) / 2))
 }
